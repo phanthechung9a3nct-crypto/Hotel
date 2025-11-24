c7892c5102bb3f78ac666e8058b0d1b6b1f9b771
phanthechung9a3nct-crypto <phanthechung9a3nct@gmail.com>
2025-11-25 01:22:09 +0700

    Update home.php

--- inc/home.php
+++ inc/home.php
@@ -78,7 +78,7 @@
 							<select class="form-select shadow-none">
 
 								<option value="1">1</option>
-								<option value="2">34</option>
+								<option value="2">2</option>
 								<option value="3">3</option>
 							</select>
 						</div>
